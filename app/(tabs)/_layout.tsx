b513d4e4419e8d9f34d0061169863777346d10ca
Emiel Cornelis <emiel.cornelis@ugent.be>
2025-06-30 13:52:12 +0200

    feat: added diagrams

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,40 @@
-import {View, Text} from 'react-native'
 import React from 'react'
 import {Tabs} from "expo-router";
-import "../globals.css"
+import {icons} from "@/constants/icons";
+import {Image, ImageBackground, View, Text} from "react-native";
 
 const _Layout = () => {
     return (
         <Tabs>
             <Tabs.Screen name="logging"  options={{
                 headerShown: false,
-                title: 'Log'
-            }} />
+                title: "Log",
+                tabBarIcon: ({focused}) => (
+                    <View className="items-center">
+                        <Image source={icons.log_icon} className="size-5"/>
+                        {focused && <Text className="text-xs text-blue-600">Log</Text>}
+                    </View>
+                )
+            }}></Tabs.Screen>
+
             <Tabs.Screen name="statistics"  options={{
                 headerShown: false,
-                title: 'Stats'
+                title: 'Stats',
+                tabBarIcon: ({focused}) => (
+                    <>
+                        <Image source={icons.stats_icon} className="size-5"></Image>
+                    </>
+                )
             }} />
+
             <Tabs.Screen name="profile"  options={{
                 headerShown: false,
-                title: 'Profile'
+                title: 'Profile',
+                tabBarIcon: ({focused}) => (
+                    <>
+                        <Image source={icons.profile_icon} className="size-5"></Image>
+                    </>
+                )
             }}/>
         </Tabs>
     )
---
+++ constants/icons.ts
@@ -0,0 +1,9 @@
+import log_icon from "@/assets/icons/log_icon.png";
+import profile_icon from "@/assets/icons/profile_icon.png";
+import stats_icon from "@/assets/icons/stats_icon.png";
+
+export const icons = {
+    log_icon,
+    profile_icon,
+    stats_icon,
+};
